8cac94f33f23247c549419c04394fc52d79508d1
Jhonattan Lima <jhonattanlima@yahoo.com.br>
2023-09-19 18:10:45 -0300

    criando contador para o desafio

---
+++ DesafioControleFluxo/src/contador.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class contador {
+	public static void main(String[] args) {
+		Scanner terminal = new Scanner(System.in);
+		System.out.println("Digite o primeiro parâmetro");
+		int parametroUm = terminal.nextInt();
+		System.out.println("Digite o segundo parâmetro");
+		int parametroDois = terminal.nextInt();
+		
+		try {
+			      
+            //chamando o método contendo a lógica de contagem
+			if (parametroUm > parametroDois){
+            throw new ParametrosInvalidosException();
+		}
+        contar(parametroUm, parametroDois);
+		}catch (ParametrosInvalidosException exception) {
+            System.out.println("O segundo parâmetro deve ser maior que o primeiro");
+			//imprimir a mensagem: O segundo parâmetro deve ser maior que o primeiro
+		}
+		
+	}
+	static void contar(int parametroUm, int parametroDois ) throws ParametrosInvalidosException {
+		int contagem = parametroDois - parametroUm;
+        //validar se parametroUm é MAIOR que parametroDois e lançar a exceção
+		for (int i = 1; i <= contagem; i++) {
+            System.out.println("Imprimindo o número " + i);
+        }
+		
+		//realizar o for para imprimir os números com base na variável contagem
+	}
+}
